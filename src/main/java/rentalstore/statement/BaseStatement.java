b0c4ee4a04f80999e94e42fdcfc4d58cd3bc7a2d
WUKN <knight.wu@oocl.com>
2018-08-26 15:19:22 +0800

    action:add abstract class BaseStatement as parent of Statament and HtmlStatement,put their common part in parent
    motivation:statement and HtmlStatement have common code

---
+++ src/main/java/rentalstore/statement/BaseStatement.java
@@ -0,0 +1,44 @@
+package rentalstore.statement;
+
+import rentalstore.Customer;
+import rentalstore.MovieFactory;
+import rentalstore.Rental;
+
+import java.util.Enumeration;
+
+/**
+ * @Author:Knight
+ * @Date:Create in 8/26/2018
+ * @Description:
+ */
+public abstract class BaseStatement {
+    public String getResult(Customer customer) throws Exception {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = customer.getRentals().elements();
+        String result = getHeaderString(customer);
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentals.nextElement();
+            thisAmount += MovieFactory.getMovie(each.getMovie().getPriceCode()).getAmount(each);
+            //add frequent renter points
+            frequentRenterPoints++;
+            //add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == MovieFactory.NEW_RELEASE) && each.getDayRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            //show figures for this rental
+            result += getRental(each, thisAmount);
+            totalAmount += thisAmount;
+        }
+        //add footer lines
+        result += getFooterString(totalAmount,frequentRenterPoints);
+        return result;
+    }
+
+    public abstract String getHeaderString(Customer customer);
+    public abstract String getRental(Rental each, double thisAmount);
+    public abstract String getFooterString(double totalAmount, int frequentRenterPoints);
+
+}
--- src/main/java/rentalstore/statement/Statement.java
+++ src/main/java/rentalstore/statement/Statement.java
@@ -11,31 +11,22 @@ import java.util.Enumeration;
  * @Date:Create in 8/26/2018
  * @Description:
  */
-public class Statement {
+public class Statement extends BaseStatement {
 
-    public String getResult(Customer customer) throws Exception {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration rentals = customer.getRentals().elements();
-        String result = "Rental Record for " + customer.getName() + "\n";
-        while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
-            thisAmount += MovieFactory.getMovie(each.getMovie().getPriceCode()).getAmount(each);
-            //add frequent renter points
-            frequentRenterPoints++;
-            //add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == MovieFactory.NEW_RELEASE) && each.getDayRented() > 1) {
-                frequentRenterPoints++;
-            }
-
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-        }
-        //add footer lines
-        result += "Amount owed is" + String.valueOf(totalAmount) + "\n";
-        result += "You earned" + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
+    @Override
+    public String getHeaderString(Customer customer) {
+        return "Rental Record for " + customer.getName() + "\n";
     }
+
+    @Override
+    public String getRental(Rental each, double thisAmount) {
+        return "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+    }
+
+    @Override
+    public String getFooterString(double totalAmount, int frequentRenterPoints) {
+        return "Amount owed is" + String.valueOf(totalAmount) + "\n"
+                + "You earned" + String.valueOf(frequentRenterPoints) + " frequent renter points";
+    }
+
 }
